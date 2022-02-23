9905ac1991ee8ec7cdbb8566b9d1fbe487e7016d
Sushil811 <70131939+Sushil811@users.noreply.github.com>
2022-02-23 11:31:14 +0530

    Add files via upload

---
+++ CsharpwithDatabase/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace CsharpwithDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(334, 72);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(167, 23);
+            this.txtusername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(334, 132);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(167, 23);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(426, 180);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(75, 28);
+            this.BtnLogin.TabIndex = 3;
+            this.BtnLogin.Text = "Login";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please Enter Your Name and Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "UserName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtusername);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CsharpwithDatabase/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CsharpwithDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            //Things neeeded
+            //1.SqlConnection
+            //2.SqlCommand
+            //3.SqlDataReader
+            
+            SqlConnection con = new SqlConnection("Data Source=Localhost;Database=CSharp Tut;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Account Where Username"+txtusername.Text+"' and Passsword= '"+txtPassword.Text+"'", con);
+            SqlDataReader sdr;
+            sdr= cmd.ExecuteReader();
+
+
+            if(sdr.Read())
+            {
+                Dashboard ds = new Dashboard();
+                ds.Show();
+                
+            }
+            else
+            {
+                MessageBox.Show("Incorrect username or Password","Message Title",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+    }
+}
